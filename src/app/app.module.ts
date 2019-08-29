b1bd5e35f2789b3e7b5c4e4b4b09b609db0a701e
oscaros <komuntuoscar@gmail.com>
2019-08-30 01:38:18 +0300

    added example

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,25 +3,26 @@ import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
-
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
 //import { RegisterPage } from '../pages/register/register';
 //import { DashboardPage } from '../pages/dashboard/dashboard';
-
 import { HttpModule } from '@angular/http';
+import { Contacts } from '@ionic-native/contacts/ngx';
 
 @NgModule({
   declarations: [
     MyApp,
-    HomePage//,
+    HomePage,
+    Contacts           
     //RegisterPage//,
     //DashboardPage
   ],
   imports: [
     BrowserModule,
     IonicModule.forRoot(MyApp),
-    HttpModule
+    HttpModule//,
+    //Contacts
   ],
   bootstrap: [IonicApp],
   entryComponents: [
@@ -33,7 +34,8 @@ import { HttpModule } from '@angular/http';
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
+    Contacts
   ]
 })
 export class AppModule {}
--- src/pages/dashboard/dashboard.ts
+++ src/pages/dashboard/dashboard.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { Contacts, Contact, ContactName, ContactField } from '@ionic-native/contacts/ngx';
+import { ToastController } from 'ionic-angular';
 
 /**
  * Generated class for the DashboardPage page.
@@ -12,14 +14,44 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 @Component({
   selector: 'page-dashboard',
   templateUrl: 'dashboard.html',
-})
+})       
 export class DashboardPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  myContacts: Contact[] = [];
+
+  constructor(public navCtrl: NavController, public navParams: NavParams, private contacts: Contacts, private toastCtrl: ToastController) {
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad DashboardPage');
+  }
+
+  loadContacts() {
+    var options = {
+      filter: '',
+      multiple: true,
+      hasPhoneNumber: true
+    };
+ 
+    this.contacts.find(['*'], options).then((contacts: Contact[]) => {
+      this.myContacts = contacts;
+    });
+  }
+ 
+  createContact() {
+    let contact: Contact = this.contacts.create();
+ 
+    contact.name = new ContactName(null, 'Albus', 'Ape');
+    contact.phoneNumbers = [ new ContactField('mobile', '12345678') ];
+    contact.save().then(
+      async () => {
+        let toast = await this.toastCtrl.create({
+          message: 'Contact added!'
+        });
+        toast.present();
+      },
+      (error: any) => console.error('Error saving contact.', error)
+    );
   }
 
 }
